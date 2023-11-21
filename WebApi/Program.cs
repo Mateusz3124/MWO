d8f2883fc053011f2db1db9a64718ea6d49c4da5
MatSobol <mateuszsobol46@gmail.com>
2023-11-21 18:56:07 +0100

    this

---
+++ WebApi/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+try
+{
+    using (StreamReader reader = new StreamReader("version"))
+    {
+        string line = reader.ReadLine();
+        Console.WriteLine(line);
+    }
+}
+catch (IOException e)
+{
+    Console.WriteLine($"An error occurred while reading the file: {e.Message}");
+}
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
